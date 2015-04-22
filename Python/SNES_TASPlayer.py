c39da8f1496b934ec616b44e3983783554aecc8a
GhostSonic <ghostsonic21@gmail.com>
2015-04-21 21:22:42 -0700

    Handle file end better in player, change com port
    We need to define that com port in a separate area at some point.

--- Python/SNES_TASPlayer.py
+++ Python/SNES_TASPlayer.py
@@ -7,7 +7,7 @@ import time
 import os
 from tkinter import filedialog, Tk
 
-ser =  serial.Serial("COM3", 115200) #Make sure you change the COM port to whatever it is on your setup.
+ser =  serial.Serial("COM4", 115200) #Make sure you change the COM port to whatever it is on your setup.
 
 root = Tk()
 root.withdraw()
@@ -24,21 +24,24 @@ print('Ready...')
 
 def main():
 	amountRead = 0
+	endOfFile = False
 	while 1:
 		#os.system("title " + ('Frame Counter: ' + str(amountRead) + '/' + str(tasFileSize))) #This is just pretty. But it seems to cause desyncs if it's active, uncomment at your own risk.
 		
-		if (convertToInt(ser.read(size=1)) == 0x12 and amountRead < tasFileSize): #Receives the byte from the Arduino upon latch
-			SNESData1 = tasFile.read(1)
-			SNESData2 = tasFile.read(1)
-			ser.write(SNESData1)
-			ser.write(SNESData2)
-			print ("%d: %s %s" % (amountRead, hex(int.from_bytes(SNESData1,'little')), hex(int.from_bytes(SNESData2,'little'))))
-			amountRead += 1
-		elif (amountRead >= tasFileSize): #This should prevent an exception at the end of file
-			print('End of File!')
-			ser.write([0x00])
-			freeze() #Freeze the script when it's over.
-			#exit() can be used here to exit it instead.
+		if (convertToInt(ser.read(size=1)) == 0x12): #Receives the byte from the Arduino upon latch
+			if (not endOfFile):
+				SNESData1 = tasFile.read(1)
+				SNESData2 = tasFile.read(1)
+				ser.write(SNESData1)
+				ser.write(SNESData2)
+				print ("%d: %s %s" % (amountRead, hex(int.from_bytes(SNESData1,'little')), hex(int.from_bytes(SNESData2,'little'))))
+				amountRead += 1
+			else:
+				ser.write([0x00])
+				ser.write([0x00]) 					# Send 0 if the end of file is reached
+			if (amountRead >= tasFileSize and not endOfFile):
+				endOfFile = True
+				print("End of File!")
 		else:
 			pass
 
